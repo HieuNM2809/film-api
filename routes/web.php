e09b397a01af8486fd1b9188a6cbb0e2f9896bc5
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-05-28 16:43:15 +0700

    Add base controller + view login, 404, register ,  home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\CreditCartController;
+use  App\Http\Controllers\DonateController;
+use App\Http\Controllers\GroupPermissionController;
+use App\Http\Controllers\HashtagController;
+use App\Http\Controllers\IconRankController;
+use App\Http\Controllers\OrganizationController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ReactController;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\TitleTypeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserFeelController;
+use App\Http\Controllers\UserIconRankController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +29,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('pages.home');
+    });
+    Route::get('login', function () {
+        return view('pages.login');
+    });
+    Route::get('register', function () {
+        return view('pages.register');
+    });
+
+    Route::resource('title-type', TitleTypeController::class);
+    Route::resource('icon-rank', IconRankController::class);
+    Route::resource('user-icon-rank', UserIconRankController::class);
+    Route::resource('settings', SettingsController::class);
+    Route::resource('comment', CommentsController::class);
+    Route::resource('post', PostController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('organization', OrganizationController::class);
+    Route::resource('user-feel', UserFeelController::class);
+    Route::resource('donate', DonateController::class);
+    Route::resource('react', ReactController::class);
+    Route::resource('group-permission', GroupPermissionController::class);
+    Route::resource('hash-tag', HashtagController::class);
+    Route::resource('permission', PermissionController::class);
+    Route::resource('credit-cart', CreditCartController::class);
+
 });
