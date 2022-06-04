9d1f716b70c0795794b8edc78e637282b1a32d7b
Hiếu Minh(hm) <nmhieucoder@gmail.com>
2022-06-04 09:55:29 +0700

    push base apiResource

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,6 +13,7 @@ class PostController extends BaseController
      */
     public function index()
     {
+        return "123213";
         return view('pages.post.list');
     }
 
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentsController;
+use App\Http\Controllers\CreditCartController;
+use  App\Http\Controllers\DonateController;
+use App\Http\Controllers\GroupPermissionController;
+use App\Http\Controllers\HashtagController;
+use App\Http\Controllers\IconRankController;
+use App\Http\Controllers\OrganizationController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ReactController;
+use App\Http\Controllers\SettingsController;
+use App\Http\Controllers\TitleTypeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserFeelController;
+use App\Http\Controllers\UserIconRankController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +39,23 @@ Route::controller(AuthController::class)->group(function () {
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::get('test', function(){
+    return 'test ok';
+});
+
+Route::apiResource('post', PostController::class);
+Route::apiResource('title-type', TitleTypeController::class);
+Route::apiResource('icon-rank', IconRankController::class);
+Route::apiResource('user-icon-rank', UserIconRankController::class);
+Route::apiResource('settings', SettingsController::class);
+Route::apiResource('comment', CommentsController::class);
+Route::apiResource('user', UserController::class);
+Route::apiResource('organization', OrganizationController::class);
+Route::apiResource('user-feel', UserFeelController::class);
+Route::apiResource('donate', DonateController::class);
+Route::apiResource('react', ReactController::class);
+Route::apiResource('group-permission', GroupPermissionController::class);
+Route::apiResource('hash-tag', HashtagController::class);
+Route::apiResource('permission', PermissionController::class);
+Route::apiResource('credit-cart', CreditCartController::class);
